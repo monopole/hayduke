f0970bf312ac8578fd4b6f19cded9db8ba4b0a87
Jeffrey Regan <jregan@google.com>
2019-10-23 12:56:39 -0700

    Take Foo to v2 (change in output)

--- foo/go.mod
+++ foo/go.mod
@@ -2,4 +2,7 @@ module github.com/monopole/hayduke/foo
 
 go 1.13
 
-require github.com/monopole/hayduke/api v1.0.0
+require (
+	github.com/monopole/hayduke/api v1.0.0
+	github.com/monopole/hayduke/api/v2 v2.0.0
+)
--- foo/main.go
+++ foo/main.go
@@ -5,8 +5,9 @@ import (
 	"fmt"
 
 	"github.com/monopole/hayduke/api/prefixer"
+	v2p "github.com/monopole/hayduke/api/v2/prefixer"
 )
 
 func main() {
- fmt.Println("Hey there " + prefixer.Prefix("bob"))
+ fmt.Println("Hey there " + prefixer.Prefix("bob") + v2p.Prefix("bob", "cheese"))
 }
